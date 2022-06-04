657fa760d08ecfb53100118f877b29dc0a2c1879
clifstone <47040820+clifstone@users.noreply.github.com>
2022-06-04 02:20:07 -0500

    committing stuff for the night. added a specific front-page template. restructuring scss files to be more scalable

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+    <div class="main-wrap">
+        <main class="site-main" role="main">
+            <?php the_content(); ?>
+            <?php if(is_active_sidebar('abovefooter')){ dynamic_sidebar('abovefooter'); } ?>
+        </main>
+    </div>
+
+<?php get_footer(); ?>
---
+++ parts/sitesidebar.php
@@ -0,0 +1,27 @@
+<?php
+    $mcThumb = get_option('aside_logo_url');
+?>
+
+<div class="site-sidebar">
+    <div class="wrapper">
+
+        <div class="sidebar-header">
+            <div class="site-logo"><a href="<?php echo home_url(); ?>"><?php echo '<img class="logo-img" src="'.$mcThumb.'" alt="site-logo" width="100%" height="auto" loading="lazy" />'; ?></a></div>
+        </div>
+        
+        <div class="nav">
+            <div class="navtitle">
+                <span>Sections</span>
+            </div>
+            <nav><?php wp_nav_menu( array( 'theme_location' => 'menu_mobile_nav', 'menu_class' => 'site-aside-menu menu' ) ); ?></nav>
+        </div>
+
+        <button id="searchbtn" class="searchbtn" aria-label="Search site">
+            <i class="i-search"></i><span>Search</span>
+        </button>
+        
+        <?php get_template_part( 'parts/social'); ?>
+        <?php get_template_part( 'parts/userlogin'); ?>
+
+    </div>
+</div>
